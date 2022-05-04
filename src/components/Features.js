69fc4755160779bf94c1d16c6b25c7a0e1c99c60
Caio Oliveira <caiolv@me.com>
2022-05-04 10:56:12 -0300

    add: finish features section

--- src/components/Features.js
+++ src/components/Features.js
@@ -8,42 +8,42 @@ export default function Features() {
           <svg className="feature__icon">
             <use href={sprite + "#icon-global"}></use>
           </svg>
-          <h4 className="heading-4">World's best luxury homes</h4>
+          <h4 className="heading-4 heading-4--dark">World's best luxury homes</h4>
           <p className="feature__text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur voluptatibus.</p>
         </div>
         <div className="feature">
           <svg className="feature__icon">
             <use href={sprite + "#icon-trophy"}></use>
           </svg>
-          <h4 className="heading-4">Only the best properties</h4>
+          <h4 className="heading-4 heading-4--dark">Only the best properties</h4>
           <p className="feature__text">Voluptatum mollitia quae. Vero ipsum sapiente molestias accusamus rerum sed a eligendi aut quia.</p>
         </div>
         <div className="feature">
           <svg className="feature__icon">
             <use href={sprite + "#icon-map-pin"}></use>
           </svg>
-          <h4 className="heading-4">all homes in top locations</h4>
+          <h4 className="heading-4 heading-4--dark">all homes in top locations</h4>
           <p className="feature__text">Tenetur distinctio necessitatibus pariatur voluptatibus quidem consequatur harum.</p>
         </div>
         <div className="feature">
           <svg className="feature__icon">
             <use href={sprite + "#icon-key"}></use>
           </svg>
-          <h4 className="heading-4">New home in one week</h4>
+          <h4 className="heading-4 heading-4--dark">New home in one week</h4>
           <p className="feature__text">Vero ipsum sapiente molestias accusamus rerum. Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
         <div className="feature">
           <svg className="feature__icon">
             <use href={sprite + "#icon-presentation"}></use>
           </svg>
-          <h4 className="heading-4">Top 1% realtors</h4>
+          <h4 className="heading-4 heading-4--dark">Top 1% realtors</h4>
           <p className="feature__text">Quidem consequatur harum, voluptatum mollitia quae. Tenetur distinctio necessitatibus pariatur voluptatibus.</p>
         </div>
         <div className="feature">
           <svg className="feature__icon">
             <use href={sprite + "#icon-lock"}></use>
           </svg>
-          <h4 className="heading-4">Secure payments on Nexter</h4>
+          <h4 className="heading-4 heading-4--dark">Secure payments on Nexter</h4>
           <p className="feature__text">Pariatur voluptatibus quidem consequatur harum, voluptatum mollitia quae.</p>
         </div>
     </section>
