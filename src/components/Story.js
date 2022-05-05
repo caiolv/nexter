df5648f3eef81bbea407324a403660791b0af641
Caio Oliveira <caiolv@me.com>
2022-05-05 16:21:37 -0300

    add: start story section

--- src/components/Story.js
+++ src/components/Story.js
@@ -7,7 +7,12 @@ export default function Story() {
             Story pictures
         </div>
         <div className="story__content">
-            Story content
+            <h3 class="heading-3 mb-sm">Happy Costumers</h3>
+            <h2 class="heading-2 heading-2--dark mb-md">&ldquo;The best decision of our lives&rdquo;</h2>
+            <p className="story__text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur distinctio necessitatibus pariatur voluptatibus. Quidem consequatur harum volupta!
+            </p>
+            <button class="btn">Find your own home</button>
         </div>
     </>
   );
