646396a4bab8001d4e3d88003e8c2af61d0928c1
Caio Oliveira <caiolv@me.com>
2022-05-12 22:52:09 -0300

    add: story pictures

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,10 +1,13 @@
 import React from "react";
+import story1 from '../images/story-1.jpeg';
+import story2 from '../images/story-2.jpeg';
 
 export default function Story() {
   return (
     <>
         <div className="story__pictures">
-            Story pictures
+            <img src={story1} alt="Couple with new house" className="story__img--1" />
+            <img src={story2} alt="New house" className="story__img--2" />
         </div>
         <div className="story__content">
             <h3 class="heading-3 mb-sm">Happy Costumers</h3>
