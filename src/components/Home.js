b8d5cd5618fd0c24daf2c875b2f4d5d703543c74
Caio Oliveira <caiolv@me.com>
2022-05-26 18:00:52 -0300

    add: finish home layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,41 @@
-import React from 'react';
-import sprite from '../images/sprite.svg';
+import React from "react";
+import sprite from "../images/sprite.svg";
 
 export default function Home({ image, name, price, rooms, area, location }) {
   return (
     <div class="home">
-    <img src={image} alt="House 1" class="home__img" />
-    <svg className="home__link">
-      <use href={sprite + "#icon-heart-full"}></use>
-    </svg>
-    <h5 class="home__name">{name}</h5>
-    <div class="home__location">
-      <svg>
-        <use href={sprite + "#icon-map-pin"}></use>
+      <img src={image} alt="House 1" class="home__img" />
+      <svg className="home__like">
+        <use href={sprite + "#icon-heart-full"}></use>
       </svg>
-      <p>{location}</p>
+      <h5 class="home__name">{name}</h5>
+      <div class="home__location">
+        <svg>
+          <use href={sprite + "#icon-map-pin"}></use>
+        </svg>
+        <p>{location}</p>
+      </div>
+      <div class="home__rooms">
+        <svg>
+          <use href={sprite + "#icon-profile-male"}></use>
+        </svg>
+        <p>{rooms} rooms</p>
+      </div>
+      <div class="home__area">
+        <svg>
+          <use href={sprite + "#icon-expand"}></use>
+        </svg>
+        <p>
+          {area} m<sup>2</sup>
+        </p>
+      </div>
+      <div class="home__price">
+        <svg>
+          <use href={sprite + "#icon-key"}></use>
+        </svg>
+        <p>{price}</p>
+      </div>
+      <button class="btn home__btn">Contact realtor</button>
     </div>
-    <div class="home__rooms">
-      <svg>
-        <use href={sprite + "#icon-profile-male"}></use>
-      </svg>
-      <p>{rooms} rooms</p>
-    </div>
-    <div class="home__area">
-      <svg>
-        <use href={sprite + "#icon-expand"}></use>
-      </svg>
-      <p>{area} m<sup>2</sup></p>
-    </div>
-    <div class="home__price">
-      <svg>
-        <use href={sprite + "#icon-key"}></use>
-      </svg>
-      <p>{price}</p>
-    </div>
-    <button class="cta">Contact realtor</button>
-  </div>
-  )
+  );
 }
